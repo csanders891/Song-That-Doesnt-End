be16ea384056529bf646138cb852fb80202c6acf
csanders891 <65356624+csanders891@users.noreply.github.com>
2025-03-27 10:56:50 -0500

    Add files via upload

---
+++ script/lambChopSong.js
@@ -0,0 +1,16 @@
+var s1 = "This is the song that doesn't end";
+var s2 = "Yes it goes on and on my friend"; 
+var s3 = "Some people started singing it not knowing what it was"; 
+var s4 = "And they'll continue singing it forever just because";
+
+//always true 
+var x = 43;
+
+//loops forever
+while (x > 31)
+    {
+        alert(s1);
+        alert(s2);
+        alert(s3);
+        alert(s4);
+    }
